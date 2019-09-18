14269694802dc3af6476f810fb25269a70cf2420
Mikhail Stolyarov <schnack.desu@gmail.com>
2019-09-18 10:48:01 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gotest
-test assistant
+Test assistant
+
+## Usage
+        
+        import (
+            "testing"
+            "github.com/schnack/gotest"
+           )
+        
+        
+        func TestExample(t *testing.T) {
+        	// pass
+        	if err := gotest.Expect(2 + 2).Eq(4); err != nil {
+        		t.Error(err)
+        	}
+        }
---
+++ gotest.go
@@ -0,0 +1,55 @@
+package gotest
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Библиотека для тестирования
+// Пример использования
+// 		if err := gotest.Expect("string111").Eq("string11"); err != nil { t.Error(err) }
+
+func Expect(expect interface{}) GoTest {
+	return &goTest{expect: expect}
+}
+
+type GoTest interface {
+	Eq(v interface{}) error
+	IsZero() error
+	NotIsZero() error
+}
+
+type goTest struct {
+	expect interface{}
+}
+
+func (gt *goTest) Eq(v interface{}) error {
+	if !reflect.DeepEqual(v, gt.expect) {
+		xSample := reflect.ValueOf(v)
+		xExpect := reflect.ValueOf(gt.expect)
+		expect := fmt.Sprintf("%v", gt.expect)
+		sample := fmt.Sprintf("%v", v)
+		if xExpect.Kind() == reflect.String {
+			expect = fmt.Sprintf(`"%s"`, expect)
+		}
+		if xSample.Kind() == reflect.String {
+			sample = fmt.Sprintf(`"%s"`, sample)
+		}
+		return fmt.Errorf("\n\nexpected: %v\n     got: %v\n\n(compared using reflect.DeepEqual)\n", sample, expect)
+	}
+	return nil
+}
+
+func (gt *goTest) IsZero() error {
+	if reflect.ValueOf(gt.expect).IsZero() {
+		return nil
+	}
+	return fmt.Errorf("\n\nIs not zero\n\n(compared using reflect.IsZero)\n")
+}
+
+func (gt *goTest) NotIsZero() error {
+	if !reflect.ValueOf(gt.expect).IsZero() {
+		return nil
+	}
+	return fmt.Errorf("\n\nIs zero\n\n(compared using reflect.IsZero)\n")
+}
